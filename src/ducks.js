9defb2d284e434566e5c15273195ef2208f39b79
Yuanhao Zhang <cercle@bu.edu>
2020-01-27 15:58:42 -0500

    give up on deleting ducks

--- src/ducks.js
+++ src/ducks.js
@@ -9,15 +9,19 @@ class Ducks extends React.Component {
   constructor() {
     super();
     this.state = {
-      ducks: {}
+      ducks: {},
+      score: 0
     };
   }
 
   componentDidMount() {
+    const AFRAME = window.AFRAME;
     const ducksWithTime = [
       { time: 0, position: { x: -0.4, y: 0.4, z: -7 } },
       { time: 1000, position: { x: 0, y: 0.4, z: -7 } },
-      { time: 2000, position: { x: 0.4, y: 0.4, z: -7 } }
+      { time: 2000, position: { x: 0.4, y: 0.4, z: -7 } },
+      { time: 3000, position: { x: -0.4, y: 0.4, z: -7 } },
+      { time: 4000, position: { x: 0, y: 0.4, z: -7 } }
     ];
     let duckNum = 0;
     ducksWithTime.forEach(duck => {
@@ -25,68 +29,64 @@ class Ducks extends React.Component {
         duckNum++;
         this.setState({
           ...this.state,
-          [`duck-${duckNum}`]: duck
+          ducks: { ...this.state.ducks, [`duck-${duckNum}`]: duck.position }
         });
       }, duck.time);
     });
-    const AFRAME = window.AFRAME;
 
-    const updateDuck = (id, z) => {
-      console.log(`Updating duck at ID ${id}`);
-      // this.setState(prevState => {
-      //   prevState.ducks = prevState.ducks.map(duck => {
-      //     console.log(duck.id, id, duck.id === id);
-      //     if (duck.id === id) {
-      //       duck.position.z = z;
-      //       console.log(z, duck);
-      //     }
-      //     return duck;
-      //   });
-      //   console.log("in the updateDuck, prevState", prevState);
-      //   return prevState;
-      // });
-      this.setState({});
-    };
+    // const updateDuck = (id, position) => {
+    //   console.log(`Updating duck at ID ${id}`);
+    //   this.setState({ ...this.state, [id]: position });
+    // };
 
-    const removeDuck = id => {
-      console.log(`Rmving diuck at ID ${id}`);
-
-      this.setState(prevState => {
-        delete prevState.ducks[id];
-        console.log("new state", prevState);
-        return prevState;
-      });
-    };
-
-    const checkCollide = duckPo => {
+    // const removeDuck = id => {
+    //   console.log(`Rmving diuck at ID ${id}`);
+    //   this.setState(prevState => {
+    //     console.log("before state", prevState);
+    //     const ducks = prevState.ducks;
+    //     delete ducks[id];
+    //     console.log("after state", prevState);
+    //     return prevState;
+    //   });
+    // };
+    let lastHitDuckId = "";
+    const checkCollide = (duckId, duckPo) => {
       const playerPo = this.props.player.current.props.position;
-      console.log(playerPo, duckPo);
-      console.log(
-        "player position check",
+
+      if (
+        duckId !== lastHitDuckId &&
         playerPo.x === Number(duckPo.x) &&
-          playerPo.z - 0.1 < Number(duckPo.z) &&
-          Number(duckPo.z) < playerPo.z + 0.1
-      );
+        playerPo.z - 0.1 < Number(duckPo.z) &&
+        Number(duckPo.z) < playerPo.z + 0.1
+      ) {
+        lastHitDuckId = duckId;
+        console.log("checking if collide", playerPo, duckPo);
+        this.setState(prev => {
+          prev.score++;
+          return prev;
+        });
+      }
     };
 
     AFRAME.registerComponent("duck-obstacle", {
       tick: function(time, timeDelta) {
         var position = this.el.object3D.position;
         var id = this.el.id;
-        updateDuck(id, Number(position.z));
+        // updateDuck(id, position);
 
-        if (position.z == 1.5) {
-          console.log(`${id}'s z index is 1.5`);
-          removeDuck(id);
-        }
-        checkCollide(position);
+        // if (position.z == 1.5) {
+        //   console.log(`${id}'s z index is 1.5`);
+        //   // removeDuck(id);
+        // }
+        checkCollide(id, position);
       }
     });
   }
 
   render() {
-    const duckKeys = Object.keys(this.state.ducks);
-    console.log(this.state);
+    const ducksObject = this.state.ducks;
+    const duckKeys = Object.keys(ducksObject);
+    console.log("in the render", this.state);
 
     return (
       <Entity id="real-container">
@@ -100,20 +100,24 @@ class Ducks extends React.Component {
               scale="0.2 0.2 0.2"
               rotation="0 -90 0"
               shadow
-              // position={`${duck.position.x} ${duck.position.y} ${duck.position.z}`}
               animation={{
                 property: "position",
                 dur: "6000",
-                from: `${this.state[duckKey].position.x} ${this.state[duckKey].position.y} ${this.state[duckKey].position.z}`,
-                to: `${this.state[duckKey].position.x} ${
-                  this.state[duckKey].position.y
-                } ${this.state[duckKey].position.z + 8.5}`
+                from: `${ducksObject[duckKey].x} ${ducksObject[duckKey].y} ${ducksObject[duckKey].z}`,
+                to: `${ducksObject[duckKey].x} ${
+                  ducksObject[duckKey].y
+                } ${ducksObject[duckKey].z + 8.5}`
               }}
-            ></Entity>
+            />
           ))
         ) : (
-          <div></div>
+          <Entity></Entity>
         )}
+
+        <Entity
+          text={{ value: `Score : ${this.state.score}`, align: "center" }}
+          position={{ x: 0, y: 2, z: 0 }}
+        />
       </Entity>
     );
   }
